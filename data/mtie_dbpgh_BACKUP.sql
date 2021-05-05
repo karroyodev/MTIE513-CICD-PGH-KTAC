a1ee2f025e204809ff2287ca000c228e89ea211a
karroyodev <isc.karroyoc@gmail.com>
2021-05-05 18:45:52 -0500

    actualización script

--- data/mtie_dbpgh_BACKUP.sql
+++ data/mtie_dbpgh_BACKUP.sql
@@ -1,9 +1,3 @@
-/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ 
-/*!80016 DEFAULT ENCRYPTION='N' */;
-/*
-CREATE DATABASE  IF NOT EXISTS `mtie_dbpgh` 
-USE `mtie_dbpgh`;
-*/
 -- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
 --
 -- Host: localhost    Database: mtie_dbpgh
@@ -275,4 +269,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-30  2:20:20
+-- Dump completed on 2021-05-05 18:42:48
