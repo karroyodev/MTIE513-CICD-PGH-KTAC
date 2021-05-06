220b197eb478a710fcf59dc79ca37ecd9e6f6bc9
karroyodev <isc.karroyoc@gmail.com>
2021-05-05 19:59:52 -0500

    delete files, update yaml

--- volumes/logstash/config/queries/incremental.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	j.journal_id, j.action_type, j.isbn,
-	b.title, b.authors, b.publication_date
-FROM books.books_journal j
-LEFT JOIN books.books b ON b.isbn = j.isbn
-WHERE j.journal_id > :sql_last_value
-	AND j.action_time < NOW()
-ORDER BY j.journal_id
